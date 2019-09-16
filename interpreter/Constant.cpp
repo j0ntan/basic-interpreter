e7d43df7861bc79297cbfa7cf8b37cf7cf370a5e
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-16 13:45:04 -0400

    Format Constant value to string

--- interpreter/Constant.cpp
+++ interpreter/Constant.cpp
@@ -4,4 +4,4 @@ Constant::Constant(int n) : val{n} {}
 
 int Constant::value() const { return val; }
 
-std::string Constant::format() const { return std::string(); }
+std::string Constant::format() const { return std::to_string(val); }
--- tests/Constant.cpp
+++ tests/Constant.cpp
@@ -14,3 +14,11 @@ TEST(Constant, valueMatchesIntegerArguement) {
   ASSERT_EQ(c1.value(), x1);
   ASSERT_EQ(c2.value(), x2);
 }
+
+TEST(Constant, formatIntegerValueToString) {
+  int x1 = 123, x2 = -8471234;
+  Constant c1(x1), c2(x2), c3;
+  ASSERT_EQ(c1.format(), "123");
+  ASSERT_EQ(c2.format(), "-8471234");
+  ASSERT_EQ(c3.format(), "0");
+}
