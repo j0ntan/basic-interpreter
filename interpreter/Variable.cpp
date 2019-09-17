853be4b8c2cfb40f461d6945a2d522022f8da6af
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-16 22:24:12 -0400

    Give default zero Variable value

--- interpreter/Variable.cpp
+++ interpreter/Variable.cpp
@@ -4,6 +4,6 @@
 Variable::Variable(std::string name, Constant value)
     : var_name{std::move(name)}, var_value{std::move(value)} {}
 
-int Variable::value() const { return -1; }
+int Variable::value() const { return 0; }
 
 std::string Variable::format() const { return std::string(""); }
--- tests/Variable.cpp
+++ tests/Variable.cpp
@@ -2,3 +2,8 @@
 #include <gtest/gtest.h>
 
 TEST(Variable, createVariable) { Variable v("name", Constant()); }
+
+TEST(Variable, defaultValueIsZero) {
+  Variable v("name", Constant());
+  ASSERT_EQ(v.value(), 0);
+}
