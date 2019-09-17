424eea3e42eddff95f32d6447c1afabde258d1ad
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-17 17:46:29 -0400

    Require line number for abstract Command class

---
+++ interpreter/Command.h
@@ -0,0 +1,18 @@
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#include <string>
+
+class Command {
+public:
+  Command(unsigned int line_number) : line_num{line_number} {}
+
+  virtual std::string format() const = 0;
+
+  virtual ~Command() = default;
+
+protected:
+  const unsigned int line_num;
+};
+
+#endif
--- interpreter/command.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMAND_HPP
-#define COMMAND_HPP
-
-class Command {
-public:
-  virtual std::string format() const = 0;
-};
-
-#endif
