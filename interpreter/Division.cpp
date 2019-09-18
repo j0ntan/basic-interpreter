e7e9e19372f4a42b0c8e41ff8ec45e5414b84c43
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-18 02:19:46 -0400

    Calculate Multiplication value

--- interpreter/Division.cpp
+++ interpreter/Division.cpp
@@ -4,4 +4,4 @@ Division::Division(const NumericExpression &nexp1,
                    const NumericExpression &nexp2)
     : BinaryExpression(nexp1, nexp2, '/') {}
 
-int Division::value() const { return -1; }
+int Division::value() const { return num1.value() / num2.value(); }
--- tests/Division.cpp
+++ tests/Division.cpp
@@ -14,3 +14,13 @@ TEST(Division, canCreateNestedDivision) {
   Division d1(num1, num2);
   Division d2(d1, num3);
 }
+
+TEST(Division, addIntegerValuesAndNested) {
+  Constant x(200), y(2), z(-40), zz(-5);
+  Division positives(x, y), positive_and_negative(x, z), negatives(z, zz);
+  Division nested(positives, zz);
+  ASSERT_EQ(positives.value(), 100);
+  ASSERT_EQ(negatives.value(), 8);
+  ASSERT_EQ(positive_and_negative.value(), -5);
+  ASSERT_EQ(nested.value(), -20);
+}
