f97ff331257a1cfcde2df2cc3b418a492e87b612
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-18 01:21:02 -0400

    Calculate Multiplication value

--- interpreter/Multiplication.cpp
+++ interpreter/Multiplication.cpp
@@ -4,4 +4,4 @@ Multiplication::Multiplication(const NumericExpression &nexp1,
                                const NumericExpression &nexp2)
     : BinaryExpression(nexp1, nexp2, '*') {}
 
-int Multiplication::value() const { return -1; }
+int Multiplication::value() const { return num1.value() * num2.value(); }
--- tests/Multiplication.cpp
+++ tests/Multiplication.cpp
@@ -14,3 +14,13 @@ TEST(Multiplication, canCreateNestedMultiplication) {
   Multiplication m1(num1, num2);
   Multiplication m2(m1, num3);
 }
+
+TEST(Multiplication, multiplyIntegerValuesAndNested) {
+  Constant x(5), y(2), z(-3);
+  Multiplication positives(x, y), positive_and_negative(x, z), negatives(z, z);
+  Multiplication nested(positives, y);
+  ASSERT_EQ(positives.value(), 10);
+  ASSERT_EQ(negatives.value(), 9);
+  ASSERT_EQ(positive_and_negative.value(), -15);
+  ASSERT_EQ(nested.value(), 20);
+}
