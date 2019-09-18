765d13fd073ca9209fee6960e5c1f7df8c5fa418
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-18 04:28:32 -0400

    Evaluate LessThan expressions

--- interpreter/LessThan.cpp
+++ interpreter/LessThan.cpp
@@ -4,4 +4,4 @@ LessThan::LessThan(const NumericExpression &nexp1,
                    const NumericExpression &nexp2)
     : BooleanExpression(nexp1, nexp2, '<') {}
 
-bool LessThan::value() const { return false; }
+bool LessThan::value() const { return num1.value() < num2.value(); }
--- tests/LessThan.cpp
+++ tests/LessThan.cpp
@@ -1,4 +1,5 @@
 #include "LessThan.h"
+#include "Addition.h"
 #include "Constant.h"
 #include "Variable.h"
 #include <gtest/gtest.h>
@@ -7,4 +8,16 @@ TEST(LessThan, canCreateLessThanOfConstantsAndVariables) {
   Constant num1, num2;
   Variable num3, num4;
   LessThan l1(num1, num2), l2(num3, num4), l3(num1, num4), l4(num3, num2);
+}
+
+TEST(LessThan, evaluateSimpleAndNestedExpressions) {
+  Constant x(123), y(-456), z(456);
+  Addition a(x, x);
+  Addition nested(a, z);
+  LessThan l1(x, y), l2(x, x), l3(y, x), l4(x, z), l5(nested, z);
+  ASSERT_FALSE(l1.value());
+  ASSERT_FALSE(l2.value());
+  ASSERT_TRUE(l3.value());
+  ASSERT_TRUE(l4.value());
+  ASSERT_FALSE(l5.value());
 }
