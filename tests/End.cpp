e6555d278c6b0fe43abf726c4a0948b67d82e97e
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-27 04:40:05 -0400

    Match given line number for End

--- tests/End.cpp
+++ tests/End.cpp
@@ -5,3 +5,9 @@ TEST(End, createSimpleEnd) {
   const unsigned int line_number = 123;
   End e(line_number);
 }
+
+TEST(End, matchGivenLineNumber) {
+  const unsigned int line_number = 123;
+  End e(line_number);
+  ASSERT_EQ(e.line(), line_number);
+}
