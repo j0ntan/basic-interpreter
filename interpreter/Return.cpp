ff10f4e5731e43711fcaa4c8cc10e385e8dc0f03
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-09-27 04:11:05 -0400

    Match given line number for Return

--- interpreter/Return.cpp
+++ interpreter/Return.cpp
@@ -1,5 +1,5 @@
 #include "Return.h"
 
-Return::Return(unsigned int line_number) : Command{0} {}
+Return::Return(unsigned int line_number) : Command{line_number} {}
 
 std::string Return::format() const { return std::string(); }
--- tests/Return.cpp
+++ tests/Return.cpp
@@ -5,3 +5,9 @@ TEST(Return, createSimpleReturn) {
   const unsigned int line_number = 123;
   Return r(line_number);
 }
+
+TEST(Return, matchGivenLineNumber) {
+  const unsigned int line_number = 123;
+  Return r(line_number);
+  ASSERT_EQ(r.line(), line_number);
+}
