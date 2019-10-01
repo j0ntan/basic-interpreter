ca23481edca7a9741b46fdefdcd89bfad7271312
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-10-01 13:06:14 -0400

    Find simple positive integer

--- interpreter/Parser/Parser.cpp
+++ interpreter/Parser/Parser.cpp
@@ -1,3 +1,12 @@
 #include "Parser.h"
+#include <cctype>
 
-std::string findNextExpression(const std::string &cmd) { return std::string(); }
+std::string findNextExpression(const std::string &cmd) {
+  size_t begins = 0;
+  while (begins < cmd.size() && !std::isdigit(cmd[begins]))
+    ++begins;
+  size_t ends = begins;
+  while (ends < cmd.size() && std::isdigit(cmd[ends++]))
+    ;
+  return cmd.substr(begins, ends);
+}
--- tests/Parser.cpp
+++ tests/Parser.cpp
@@ -2,3 +2,7 @@
 #include <gtest/gtest.h>
 
 TEST(findNextExpression, callGetNextExpression) { findNextExpression("123"); }
+
+TEST(findNextExpression, findSimplePositiiveInteger) {
+  ASSERT_EQ(findNextExpression("123"), "123");
+}
