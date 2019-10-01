6499388f904484b4031719c9e208b1cd3ec56313
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-10-01 14:20:45 -0400

    Refactor 'findNextExpression'

--- interpreter/Parser/Parser.cpp
+++ interpreter/Parser/Parser.cpp
@@ -2,14 +2,10 @@
 #include <cctype>
 
 std::string findNextExpression(const std::string &cmd) {
-  size_t begins = 0;
-  while (begins < cmd.size() &&
-         !(std::isdigit(cmd[begins]) || cmd[begins] == '-' ||
-           std::isupper(cmd[begins])))
-    ++begins;
-  size_t ends = begins + 1;
-  while (ends < cmd.size() &&
-         (std::isdigit(cmd[ends]) || std::isupper(cmd[ends])))
-    ++ends;
+  size_t begins = 0, ends = 0;
+  if (std::isdigit(cmd[0]) || cmd[0] == '-')
+    ends = cmd.find_first_not_of("-0123456789");
+  else if (std::isupper(cmd[0]))
+    ends = cmd.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
   return cmd.substr(begins, ends);
 }
