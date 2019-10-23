2deef3afc0c56fe09a507ba943c3da160f6036c5
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-10-23 12:35:41 -0400

    Replace BooleanExpression reference to NumericExpression with pointer

--- interpreter/BooleanExpressions/BooleanExpression.h
+++ interpreter/BooleanExpressions/BooleanExpression.h
@@ -2,24 +2,25 @@
 #define BOOLEANEXPRESSION_HPP
 
 #include "NumericExpression.h"
+#include <memory>
 #include <string>
 
 class BooleanExpression {
 public:
-  BooleanExpression(const NumericExpression &left,
-                    const NumericExpression &right, const char operation_type)
+  BooleanExpression(const NumericExpression *left,
+                    const NumericExpression *right, const char operation_type)
       : left{left}, right{right}, operation{operation_type} {}
 
   virtual bool value() const = 0;
   std::string format() const {
-    return left.format() + ' ' + operation + ' ' + right.format();
+    return left->format() + ' ' + operation + ' ' + right->format();
   }
 
   virtual ~BooleanExpression() = default;
 
 protected:
-  const NumericExpression &left;
-  const NumericExpression &right;
+  const std::unique_ptr<const NumericExpression> left;
+  const std::unique_ptr<const NumericExpression> right;
   const char operation;
 };
 
