0d4b8e861daeccf106afd2e91c5ceda4390081b4
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-10-29 00:19:30 -0400

    Setup parse & write main application

--- parse-and-write.cpp
+++ parse-and-write.cpp
@@ -6,11 +6,16 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    cerr << "Please provide an input file." << endl;
+    cerr << "Please provide an input file.\n";
     return 1;
   }
 
   ifstream input(argv[1]);
+  if (input.bad()) {
+    cerr << "Cannot open input file.\n";
+    return 1;
+  }
+
   Interpreter interpreter(input);
   interpreter.write(cout);
   return 0;
