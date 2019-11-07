47d4a36214098a95d0d600546f230d955198b79a
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-11-07 03:39:25 -0500

    Refactor whitespace formatting

--- src/Parser/FormatStringSpacing.cpp
+++ src/Parser/FormatStringSpacing.cpp
@@ -1,8 +1,21 @@
 #include "FormatStringSpacing.hpp"
 
 std::string fix_spacing(const std::string &str) {
-  const auto first_char_position = str.find_first_not_of(" \t");
-  const auto last_char_position = str.find_last_not_of(" \t");
-  return str.substr(first_char_position,
-                    last_char_position - first_char_position + 1);
+  std::string result;
+  size_t word_begins_position = str.find_first_not_of(" \t"),
+         word_ends_position;
+  while (word_begins_position < std::string::npos) {
+    word_ends_position = str.find_first_of(" \t", word_begins_position);
+    std::string word;
+    if (word_ends_position == std::string::npos) {
+      word = str.substr(word_begins_position);
+      word_begins_position = std::string::npos;
+    } else {
+      word = str.substr(word_begins_position,
+                        word_ends_position - word_begins_position);
+      word_begins_position = str.find_first_not_of(" \t", ++word_ends_position);
+    }
+    result.append(word);
+  }
+  return result;
 }
