cd94b9b06b5c1d2489ae594b6209920087d0c258
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-11-11 10:49:43 -0500

    Check string contains only digits

--- src/Parser/QueryStringProperties.cpp
+++ src/Parser/QueryStringProperties.cpp
@@ -1,3 +1,5 @@
 #include "QueryStringProperties.hpp"
 
-bool has_only_digits(const std::string &str) { return true; }
+bool has_only_digits(const std::string &str) {
+  return str.find_first_not_of("0123456789") == std::string::npos;
+}
--- src/Parser/QueryStringProperties.test.cpp
+++ src/Parser/QueryStringProperties.test.cpp
@@ -4,3 +4,5 @@
 TEST(HasOnlyDigits, callFunction) { has_only_digits("123"); }
 
 TEST(HasOnlyDigits, getBoolResult) { ASSERT_TRUE(has_only_digits("123")); }
+
+TEST(HasOnlyDigits, detectNonDigit) { ASSERT_FALSE(has_only_digits("12A3")); }
