93db1fac3507cbb9e162e65815974b90a555e502
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-11-11 18:11:19 -0500

    Document whitespace format function

--- src/Parser/FormatStringSpacing.hpp
+++ src/Parser/FormatStringSpacing.hpp
@@ -3,6 +3,14 @@
 
 #include <string>
 
+/**
+ * @brief Removes leading & trailing whitespace in a string, and replaces each
+ * continuous sequence of whitespace characters in the string with a single
+ * space
+ *
+ * @param str The string to be modified
+ * @return std::string The reduced-space string result
+ */
 std::string fix_spacing(const std::string &str);
 
 #endif
