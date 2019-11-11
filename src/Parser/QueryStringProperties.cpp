3d04f17ed023964d6c7501f4274b95d97754c426
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-11-11 10:58:53 -0500

    Get bool value on uppercase-letters-only string

--- src/Parser/QueryStringProperties.cpp
+++ src/Parser/QueryStringProperties.cpp
@@ -4,4 +4,4 @@ bool has_only_digits(const std::string &str) {
   return str.find_first_not_of("0123456789") == std::string::npos;
 }
 
-void has_only_uppercase_letters(const std::string &str) {}
+bool has_only_uppercase_letters(const std::string &str) { return true; }
--- src/Parser/QueryStringProperties.test.cpp
+++ src/Parser/QueryStringProperties.test.cpp
@@ -10,3 +10,7 @@ TEST(HasOnlyDigits, detectNonDigit) { ASSERT_FALSE(has_only_digits("12A3")); }
 TEST(HasOnlyUppercaseLetters, callFunction) {
   has_only_uppercase_letters("ABC");
 }
+
+TEST(HasOnlyUppercaseLetters, getBoolResult) {
+  ASSERT_TRUE(has_only_uppercase_letters("ABC"));
+}
