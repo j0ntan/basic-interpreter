c9a84bf354a6519dae29bd2945257cebd10c346d
Jonathan Gamboa <jgamboa0209@gmail.com>
2019-11-27 04:46:31 -0500

    Refactor getting integer expression string

--- src/Parser/GetExpressionString.cpp
+++ src/Parser/GetExpressionString.cpp
@@ -1,6 +1,10 @@
 #include "GetExpressionString.hpp"
+#include <cctype>
 
 std::string get_first_expression_str(const std::string &str) {
-  const size_t length = str.find_first_not_of("0123456789");
+  const char &first_letter = str[0];
+  size_t length;
+  if (isdigit(first_letter))
+    length = str.find_first_not_of("0123456789");
   return str.substr(0, length);
 }
